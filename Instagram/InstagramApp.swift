30cebf787ce89c0fdde73969107b4145d2a5a1e6
Edgar Vergara <edgarvm13@hotmail.com>
2023-08-07 14:42:31 -0600

    Initial Commit

---
+++ Instagram/InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramApp.swift
+//  Instagram
+//
+//  Created by Edgar Vergara on 07/08/23.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
